6b8b7f52b02819071f496b4d2be93265145fb965
Atsushi NIWAYAMA <niwayama_atsushi@infonic.co.jp>
2018-05-21 10:04:35 +0900

    余計な改行の削除、変数を関数内で入れた

--- app/weather.py
+++ app/weather.py
@@ -2,12 +2,11 @@ import urllib3
 import json
 
 #お天気の情報を取得
-http = urllib3.PoolManager()
-r = http.request('Get','http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
-r = json.loads(r._body)
-
-date = r['forecasts'][0]['date']
-weather = r['forecasts'][0]['telop']
-
 def weather_infomation():
+    http = urllib3.PoolManager()
+    r = http.request('Get','http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+    r = json.loads(r._body)
+
+    date = r['forecasts'][0]['date']
+    weather = r['forecasts'][0]['telop']
     return date + "\n" + "東京のお天気は" + weather
