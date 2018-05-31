bd3fd601590b052d6545d68faa2a55eaa0d394cc
Atsushi NIWAYAMA <niwayama_atsushi@infonic.co.jp>
2018-05-31 13:31:19 +0900

    cronは本番環境にて８時に動作したためこのままでいきます。 scheduleは本日の予定を言ってくれる機能です。

--- app/weather.py
+++ app/weather.py
@@ -1,5 +1,13 @@
 import urllib3
 import json
+#pushメッセージ送る上で必要なインポート
+from linebot.models import (TextSendMessage)
+from linebot import (LineBotApi)
+import mysql.connector
+import config
+import function as func
+
+line_bot_api = LineBotApi(config.ACCESS_TOKEN)
 
 #お天気の情報を取得
 def weather_information():
@@ -10,4 +18,14 @@ def weather_information():
     date = r['forecasts'][0]['dateLabel']
     weather = r['forecasts'][0]['telop']
     temp = r['forecasts'][0]['temperature']['max']['celsius']
-    return date + "の東京の天気は" + weather + "\n" + "最高気温は" + temp + "度"
+    weather_news = date + "の東京の天気は" + weather + "\n" + "最高気温は" + temp + "度"
+    return weather_news
+#cron用の天気予報
+def morning_news():
+    weather_info = weather_information()
+    rows = func.get_sql_send_id()
+    if rows:
+        for row in rows:
+            line_bot_api.push_message(row['send_id'], TextSendMessage(text= weather_info + "だぽん"))
+    
+morning_news()
